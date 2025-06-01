770023c8b2764eb3bc2ff2b95ff606d5fb74222c
key1ime <mtg.slate@gmail.com>
2025-06-01 17:15:25 -0400

    added pink_garnet

---
+++ src/main/java/net/key1ime/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.key1ime.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.key1ime.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+        });
+    }
+}
